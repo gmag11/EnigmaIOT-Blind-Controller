ab2c445eff15dc4097457e6bb13a0c783c5f46f1
Germán Martín <gmag11@gmail.com>
2020-05-25 14:45:39 +0200

    Remove serial commands

--- EnigmaIOT-Blind-Controller.ino
+++ EnigmaIOT-Blind-Controller.ino
@@ -26,20 +26,7 @@
 #include <Hash.h>
 #include <DNSServer.h>
 
-#include <SerialCommands.h>
-
 #define BLUE_LED LED_BUILTIN
-
-void cmd_up_cb (SerialCommands* sender);
-void cmd_down_cb (SerialCommands* sender);
-void cmd_pos_cb (SerialCommands* sender);
-void cmd_stop_cb (SerialCommands* sender);
-char serial_buffer[32];
-SerialCommands commands (&Serial, serial_buffer, sizeof (serial_buffer), "\r\n", " ");
-SerialCommand cmd_up ("UU", cmd_up_cb);
-SerialCommand cmd_down ("DD", cmd_down_cb);
-SerialCommand cmd_pos ("POS", cmd_pos_cb);
-SerialCommand cmd_stop ("STOP", cmd_stop_cb);
 
 BlindController blindController;
 
@@ -76,39 +63,6 @@ void processRxData (const uint8_t* mac, const uint8_t* buffer, uint8_t length, n
     }
 }
 
-void cmd_unrecognized (SerialCommands* sender, const char* cmd) {
-    sender->GetSerial ()->printf ("Unrecognized command [%s]\n", cmd);
-}
-
-void cmd_up_cb (SerialCommands* sender) {
-    sender->GetSerial ()->println ("UP COMMAND");
-    blindController.fullRollup ();
-}
-
-void cmd_down_cb (SerialCommands* sender) {
-    sender->GetSerial ()->println ("DOWN COMMAND");
-    blindController.fullRolldown ();
-}
-
-void cmd_pos_cb (SerialCommands* sender) {
-    char* pos_str = sender->Next ();
-    if (pos_str == NULL) {
-        sender->GetSerial ()->println ("ERROR NO POSITION");
-        return;
-    }
-    int pos = atoi (pos_str);
-    if (pos > 100 || pos < 0) {
-        sender->GetSerial ()->printf ("WRONG POSITION %d\n", pos);
-        return;
-    }
-    sender->GetSerial ()->printf ("POS COMMAND: %d\n", pos);
-    blindController.gotoPosition (pos);
-}
-
-void cmd_stop_cb (SerialCommands* sender) {
-    sender->GetSerial ()->println ("STOP COMMAND");
-    blindController.requestStop ();
-}
 
 void setup() {
     Serial.begin (115200);
@@ -124,20 +78,13 @@ void setup() {
 
     EnigmaIOTNode.begin (&Espnow_hal, NULL, NULL, true, false);
 
-    //blindController.setEventManager (processBlindEvent);
     blindController.onJsonSend (sendJson);
     blindController.begin ();
 
-    commands.SetDefaultHandler (cmd_unrecognized);
-    commands.AddCommand (&cmd_up);
-    commands.AddCommand (&cmd_down);
-    commands.AddCommand (&cmd_pos);
-    commands.AddCommand (&cmd_stop);
 }
 
 
 void loop() {
-    commands.ReadSerial ();
     blindController.loop ();
     EnigmaIOTNode.handle ();
 }
